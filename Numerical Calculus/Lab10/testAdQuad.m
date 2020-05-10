3a2ef1d00411eece7661a42f7b8896e2b430323a
Ioana Chelaru <ioanachelaru05@gmail.com>
2020-05-10 13:16:49 +0300

    Completed task in Lab10, Numerical calculus

---
+++ Numerical Calculus/Lab10/testAdQuad.m
@@ -0,0 +1,17 @@
+﻿% test functie adaptive pentru calculul unei Integrale
+function testAdQuad()
+
+    % se calculeaza integrala de la 0 la pi din sin(x)dx, cu eps = 10^(-2)
+    
+    % f - integrala
+    f = @(x) sin(x);
+    
+    % a, b - capele integralei
+    a = 0;
+    b = pi;
+    
+    epsi = 10e-2;
+    
+    % apelul functiei
+    I = problema_5(f,a,b,epsi)
+end
---
+++ Numerical Calculus/Lab10/testProblemele_1_3.m
@@ -0,0 +1,14 @@
+﻿function  test()
+    
+    dreptunghi = dreptunghi(@sin,0,pi,10)
+    
+    trapez = trapez(@sin,0,pi,10)
+    
+    simpson = simpson(@sin,0,pi,10)
+    
+    adaptive_quad_dreptunghi = problema_3(@sin,0,pi,eps,@dreptunghi)
+    
+    adaptive_quad_simpson = problema_3(@sin,0,pi,eps,@simpson)
+    
+    adaptive_quad_trapez = problema_3(@sin,0,pi,eps,@trapez)
+end
